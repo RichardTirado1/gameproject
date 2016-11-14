e759779bb61d590300019437d5136a95aebd7a5d
Richard Tirado <richardtirado1@yahoo.com>
2016-11-14 10:45:04 -0800

    PART 2 done

--- gameproject.js
+++ gameproject.js
@@ -1,6 +1,7 @@
 //PART 2
 
 var xpos = 250
+var ypos = 20
 document.addEventListener("keydown", function(e) {
  if (e.keyCode == 37) {
 document.getElementById("dog").setAttribute("x", xpos-15)
@@ -10,5 +11,12 @@ document.getElementById("dog").setAttribute("x", xpos-15)
 document.getElementById("dog").setAttribute("x", xpos+15)
   xpos = xpos + 15
 }
-
+if (e.keyCode == 38) {
+document.getElementById("dog").setAttribute("y", ypos-15)
+ ypos = ypos - 15
+}
+if (e.keyCode == 40) {
+document.getElementById("dog").setAttribute("y", ypos+15)
+ ypos = ypos + 15
+}
  })
