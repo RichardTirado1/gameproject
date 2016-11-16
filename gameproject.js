4b6f3b49becfbe4ec4009c67bc08b388b8665880
Richard Tirado <richardtirado1@yahoo.com>
2016-11-16 08:57:33 -0800

    HELP

--- gameproject.js
+++ gameproject.js
@@ -4,19 +4,19 @@ var xpos = 250
 var ypos = 20
 document.addEventListener("keydown", function(e) {
  if (e.keyCode == 37) {
-document.getElementById("dog").setAttribute("x", xpos-15)
+document.getElementById("dog").setAttribute("x", xpos - 15)
  xpos = xpos - 15
 }
  if (e.keyCode == 39) {
-document.getElementById("dog").setAttribute("x", xpos+15)
+document.getElementById("dog").setAttribute("x", xpos + 15)
   xpos = xpos + 15
 }
 if (e.keyCode == 38) {
-document.getElementById("dog").setAttribute("y", ypos-15)
+document.getElementById("dog").setAttribute("y", ypos - 15)
  ypos = ypos - 15
 }
 if (e.keyCode == 40) {
-document.getElementById("dog").setAttribute("y", ypos+15)
+document.getElementById("dog").setAttribute("y", ypos + 15)
  ypos = ypos + 15
 }
  })
